dd7fc852db1f8b0629ecf640be7d980454db6c90
RN <108650959+NakamuraRyo0304@users.noreply.github.com>
2023-03-09 18:20:07 +0900

    プレイヤー追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] int moveSpeed;
+    [SerializeField] int jumpForce;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 force = new Vector2(0, jumpForce);  //yé≤ï˚å¸ÇÃÇ›êîílÇâ¡Ç¶ÇÈ
+            rigidbody.AddForce(force);  //ÉWÉÉÉìÉv
+        }
+    }
+}
