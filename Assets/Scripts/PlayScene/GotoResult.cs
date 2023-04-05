7e1398f146be07383896f09ccc4fd573c71215f6
108650959+NakamuraRyo0304@users.noreply.github.com <108650959+NakamuraRyo0304@users.noreply.github.com>
2023-04-05 11:43:53 +0900

    整理時誤ってスクリプトを削除したため、再度追加 リザルトシーンへ移るスクリプト追加

---
+++ Assets/Scripts/PlayScene/GotoResult.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoResult : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.LoadScene("ResultScene");
+    }
+}
