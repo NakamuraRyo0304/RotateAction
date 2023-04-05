d3f0fea7efe4414feb2789513f920c926636c6cc
MitsukiShoji1204 <1204mitsuki@gmail.com>
2023-04-05 13:04:00 +0900

    バグなおした

--- Assets/Scripts/PlayScene/EffectController.cs
+++ Assets/Scripts/PlayScene/EffectController.cs
@@ -6,15 +6,11 @@ public class EffectController : MonoBehaviour
 {
     [SerializeField]
     ParticleSystem fallEffect;
-    [SerializeField]
-    GameObject RootObject;
-
     public GameObject player;
     private Vector3 effectPosition;
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.transform.parent = RootObject.gameObject.transform;
     }
 
     // Update is called once per frame
