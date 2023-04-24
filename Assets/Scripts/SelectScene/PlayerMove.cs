18b0b7a67b2f919e7ad1d0ef85c3c9609fa38691
108650959+NakamuraRyo0304@users.noreply.github.com <108650959+NakamuraRyo0304@users.noreply.github.com>
2023-04-24 09:43:41 +0900

    セレクトシーンの変更

--- Assets/Scripts/SelectScene/PlayerMove.cs
+++ Assets/Scripts/SelectScene/PlayerMove.cs
@@ -14,18 +14,23 @@ public class PlayerMove : MonoBehaviour
         switch (StageSelect.StageNum)
         {
             case 1:case 6:case 11:case 16:case 21:
+            case 26:case 31:case 36:case 41:
                 transform.position = PlayerStagePos[0].transform.position;
                 break;
             case 2:case 7:case 12:case 17:case 22:
+            case 27:case 32:case 37:case 42:
                 transform.position = PlayerStagePos[1].transform.position;
                 break;
             case 3:case 8:case 13:case 18:case 23:
+            case 28:case 33:case 38:case 43:
                 transform.position = PlayerStagePos[2].transform.position;
                 break;
             case 4:case 9:case 14:case 19:case 24:
+            case 29:case 34:case 39:case 44:
                 transform.position = PlayerStagePos[3].transform.position;
                 break;
             case 5:case 10:case 15:case 20:case 25:
+            case 30:case 35:case 40:case 45:
                 transform.position = PlayerStagePos[4].transform.position;
                 break;
 
