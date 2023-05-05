412dfb7e82c3618ffffd94eca0ae8194fbfbc3fa
MitsukiShoji1204 <1204mitsuki@gmail.com>
2023-05-06 01:22:18 +0900

    メニューの見た目完成

--- Assets/Scripts/PlayScene/MenuController.cs
+++ Assets/Scripts/PlayScene/MenuController.cs
@@ -3,15 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public int menuNum;
+    public static int menuNum;
     Vector2[] playerPos = new Vector2[4];
 
     // Start is called before the first frame update
     void Start()
     {
         menuNum = 1;
-        playerPos[0] = new Vector2(0.0f, 2.37f);
-        playerPos[1] = new Vector2(0.0f, 1.06f);
+        playerPos[0] = new Vector2(0.0f, 2.16f);
+        playerPos[1] = new Vector2(0.0f, 0.88f);
         playerPos[2] = new Vector2(0.0f, -0.21f);
         playerPos[3] = new Vector2(0.0f, -1.7f);
     }
