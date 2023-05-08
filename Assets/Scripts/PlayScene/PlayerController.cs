5654bd862378cdc41e7228ba4c9fb50e414b22c1
108650959+NakamuraRyo0304@users.noreply.github.com <108650959+NakamuraRyo0304@users.noreply.github.com>
2023-05-08 18:36:45 +0900

    プレイヤーコントローラーの無駄なコード削除

--- Assets/Scripts/PlayScene/PlayerController.cs
+++ Assets/Scripts/PlayScene/PlayerController.cs
@@ -58,11 +58,7 @@ public class PlayerController : MonoBehaviour
         if(effectflag == true)
         {
             GetDistance();
-
-            if (posDistance <= 30.0f)
-            {
-
-            }
+          
             effectTimer += 1;
 
         }
