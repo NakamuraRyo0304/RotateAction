c2cb459ff0d604e8591aace4b60a394943585215
MitsukiShoji1204 <1204mitsuki@gmail.com>
2023-05-17 10:18:30 +0900

    リスタートを追加

--- Assets/Scripts/PlayScene/Goal.cs
+++ Assets/Scripts/PlayScene/Goal.cs
@@ -16,22 +16,9 @@ public class Goal : MonoBehaviour
 
     void Start()
     {
-        // ！追記---------------------------------------------------
-        //fadeCanvas = GameObject.FindGameObjectWithTag("Fade");
-        //// 見つけてフェードスタート
-        //fadeCanvas.GetComponent<FadeManager>().FadeIn();
-
         isGoalFlag = false;
         //----------------------------------------------------------
     }
-
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if(collision.transform.tag == "Player")
-    //    {
-    //        isGoalFlag = true;
-    //    }
-    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -45,20 +32,5 @@ public class Goal : MonoBehaviour
     void Update()
     {
 
-        // FIXED: 現在、ゴールした瞬間フェードアウトが始まる
-        if (isGoalFlag)
-        {
-            // フェードアウト
-            //fadeCanvas.GetComponent<FadeManager>().FadeOut();
-        }
-
-        // FIXED: 現在、フェードアウトが終わったらセレクトに戻るようになっている
-
-        //if (fadeCanvas.GetComponent<FadeManager>().Alpha() == 1.0f)
-        //{
-        //    SceneManager.LoadScene("SelectScene");
-        //}
     }
-
- 
 }
