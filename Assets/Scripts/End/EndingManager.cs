47e2ad6290e497c0ebb2d120319c7264f9dc96ac
108650959+NakamuraRyo0304@users.noreply.github.com <108650959+NakamuraRyo0304@users.noreply.github.com>
2023-05-20 14:57:25 +0900

    シーン追加とマネージャー

--- Assets/Scripts/End/EndingManager.cs
+++ Assets/Scripts/End/EndingManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class EndingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+
     void Start()
     {
-        
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
